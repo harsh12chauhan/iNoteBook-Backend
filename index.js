e7c58cda730b1ceb3219d2c1ab53065293025a08
harsh12chauhan <harsh.chauhan@ssipmt.com>
2022-01-27 13:31:27 +0530

    added routes and models

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+// Avaiable  Routes
 app.get('/', (req, res) => {
   res.send('Hello harsh!')
 })
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ models/Notes.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title:{
+        type=String,
+        require=true
+        
+    },
+    decripton:{
+        type=String,
+        require=true
+    },
+    tag:{
+        type=String,
+        default:general
+    },
+    date:{
+        type=Date,
+        require=Date.now
+    }
+    
+  });
+  module.exports=mongoose.model('notes',NotesSchema);
