c759bec1f0eafca79bb1c645ceb035883be4a578
harsh12chauhan <harsh.chauhan@ssipmt.com>
2022-02-01 16:24:15 +0530

    modified all files untill adding note

--- models/User.js
+++ models/User.js
@@ -3,24 +3,22 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name:{
-        type:String,
-        require:true
-        
+        type: String,
+        required: true
     },
     email:{
-        type:String,
-        require:true,
-        unique:true,
+        type: String,
+        required: true,
+        unique: true
     },
     password:{
-        type:String,
-        require:true
+        type: String,
+        required: true
     },
     date:{
-        type:Date,
-        require:Date.now
-    }
-    
+        type: Date,
+        default: Date.now
+    },
   });
-  const User =  mongoose.model('user',UserSchema);
-  module.exports =User;
+  const User = mongoose.model('user', UserSchema);
+  module.exports = User;
